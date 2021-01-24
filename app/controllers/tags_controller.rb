f27c1b52f1ccdefd0783ff511177ec723651db17
Tim Flood <tflood9@gmail.com>
2021-01-23 20:28:38 -0500

    commit tree c283f1095c3dd9ebcd2e06a8419aa1f7738ee651

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,15 +1,24 @@
 class TagsController < ApplicationController
 
-  # GET /tags
-  # GET /tags.json
   def index
-    @tags = Tag.all
+    @tag = Tag.all
   end
 
-  # GET /tags/1
-  # GET /tags/1.json
   def show
     @tag = Tag.find(params[:id])
   end
 
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
